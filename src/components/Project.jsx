719a8076fa40ad0e7d23f21986fea4bcff9cbe30
JudahL <judahdslucas@gmail.com>
2018-08-21 19:21:30 +0100

    Added images and updated project component layout for projects page

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,18 +1,23 @@
 import React from "react";
 
 export default function Project(props) {
-  const { title, tech, description } = props;
+  const { title, image, tech, description } = props;
   return (
     <div className="Content-container">
-      <h3 className="Content-title">
-        {title}
-      </h3>
-      <h4 className="Content-subtitle">
-        {tech}
-      </h4>
-      <p className="Content-paragraph">
-        {description}
-      </p>
+      <div className="Project-image-container">
+        <img src={image} className="Project-image" />
+      </div>
+      <div className="Project-information-container">
+        <h3 className="Content-title">
+          {title}
+        </h3>
+        <h4 className="Content-subtitle">
+          {tech}
+        </h4>
+        <p className="Content-paragraph">
+          {description}
+        </p>
+      </div>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,25 +1,34 @@
 import React from "react";
 import SecondaryTitle from "./SecondaryTitle";
 import Project from "./Project";
+import StudyNinjaImage from "../images/icon_7.png";
+import EnterpriseGameImage from "../images/EnterpriseGame.jpg";
+import PortfolioSiteImage from "../images/PortfolioSite.jpg";
+import ObjectivesAppImage from "../images/ObjectivesApp.jpg";
+import Placeholder from "../images/logo.svg";
 
 const PROJECTS = [
   {
     title: 'DAILY OBJECTIVES APP',
+    image: ObjectivesAppImage,
     tech: 'React, Redux, Node.js, Express, MongoDB, CSS',
     description: 'A full-stack app for setting up daily objectives.'
   },
   {
     title: 'PORTFOLIO SITE',
+    image: PortfolioSiteImage,
     tech: 'React, CSS, GitHub pages',
     description: 'This portfolio site was created by myself using React and GitHub pages.'
   },
   {
     title: 'THE ENTERPRISE GAME',
+    image: EnterpriseGameImage,
     tech: 'Unity, C#',
     description: 'An educational game about running businesses based on an existing boardgame.'
   },
   {
     title: 'STUDY NINJA',
+    image: StudyNinjaImage,
     tech: 'Unity, C#',
     description: 'A study app for scheduling exam study timetables, motivational messages and learning-focused mini-games.'
   },
@@ -32,7 +41,7 @@ export default function Projects() {
       <SecondaryTitle text="PROJECTS" />
       <div className="Content">
         {PROJECTS.map(project => {
-          return <Project title={project.title} tech={project.tech} description={project.description} />
+          return <Project title={project.title} image={project.image} tech={project.tech} description={project.description} />
         })}
       </div>
     </React.Fragment>
